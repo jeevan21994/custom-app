9558981cf290f1ff17547f5df4553e9a2ecfeb10
jeevan21994 <jeevan21994>
2020-05-05 16:21:59 +0530

    add searching

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,25 +1,24 @@
 <template>
   <div id="productlist">
-
     <div class="container">
         <div class="row">
           <div class="col-sm-2 col-md-2"></div>
           <div class="col-sm-8 col-md-8">
               <div class="container">
                   <h2>Product List Table</h2>
-
-                  <table class="table table-dark table-striped ">
+                  <!-- <input @keyup="search" id="search" type="search" required v-model="keyword"> -->
+                  <input type="text" v-model="keyword" class="form-control" id="search" placeholder="Search..."/>
+                  <table class="table table-dark table-striped" >
                     <thead>
                       <tr>
                         <th>Sno</th>
                         <th>Name</th>
                         <th>Age</th>
                         <th>Action</th>
-
                       </tr>
                     </thead>
                     <tbody>
-                          <tr v-for="(item,index) in allProductsList" :key="item.id">
+                          <tr v-for="(item,index) in blogs" :key="item.id">
                           <td>{{index}}</td>
                           <td>{{item.name}}</td>
                           <td>{{item.age}}</td>
@@ -49,11 +48,19 @@
   export default{
       name:'productlist',
     
-      computed:mapGetters(["allProductsList"]),
+      computed:{
+        ...mapGetters(["allProductsList"]),
+        blogs : function(){//searching
+          return this.allProductsList.filter((blog)=>{
+            return blog.name.match(this.keyword)
+          })
+        }
+    },
       
       data(){
           return {
-            
+            keyword: '',
+            name:''
           }
       },
       methods:{
@@ -61,12 +68,12 @@
           goingComponent:function(){
           this.$router.replace('/productadd')
       },
-      
+
 
       editProduct(item){
         // this.$router.replace({ name: "productupdate", params: {id:item.id}, query: {q1: "q1"} })
         this.$router.replace({ name: "productupdate", params: {id:item.id} })
-          this.$store.dispatch('edit', item)
+          // this.$store.dispatch('edit', item)
       },
       deleteProduct:function(id){
       this.allProductsList.splice(this.allProductsList.indexOf(id), 1);
